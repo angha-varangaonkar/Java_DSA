359a5ea506c9137e76e0ce519f9aa1f3b88c6f1a
Angha Varanagaonkar <118808719+angha-varangaonkar@users.noreply.github.com>
2024-08-06 09:15:17 +0530

    Add files via upload

---
+++ Day_1/lowercase.java
@@ -0,0 +1,9 @@
+public class lowercase {
+    public static void main(String[]args)
+    {
+        String angha="my name is angha";
+        System.out.println(angha.toUpperCase());
+        System.out.println(angha.toLowerCase());
+
+    }
+}
