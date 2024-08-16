15ec82468049e3105b03a3bd70dd63844a94675f
Angha Varanagaonkar <118808719+angha-varangaonkar@users.noreply.github.com>
2024-08-16 13:06:13 +0530

    Delete Day_1 directory

--- Day_1/lowercase.java
+++
@@ -1,9 +0,0 @@
-public class lowercase {
-    public static void main(String[]args)
-    {
-        String angha="my name is angha";
-        System.out.println(angha.toUpperCase());
-        System.out.println(angha.toLowerCase());
-
-    }
-}
